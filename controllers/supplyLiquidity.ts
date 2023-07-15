f78a77f4536e13505c0cc7fd91fd2ada5b328dd2
MutebiAlvin <mutebialvinalvienzo@gmail.com>
2023-07-15 12:14:19 +0300

    Add Node Cron to PNL

--- controllers/supplyLiquidity.ts
+++ controllers/supplyLiquidity.ts
@@ -3,6 +3,7 @@ import "@nomiclabs/hardhat-ethers";
 import { ABI } from "../ABI/ABI";
 import { configParams, wallet } from "../config/config";
 import { getBalanceLinkToken } from "./getBalance";
+import cron from "node-cron";
 
 
 export async function supplyLiquidity(token_address: string, amount: any) {
@@ -13,26 +14,29 @@ export async function supplyLiquidity(token_address: string, amount: any) {
         let message = `You supplied liquidity of a  Token worth ${ethers.utils.formatEther(amount)}\n`
         message += `\n View Transaction: https://sepolia.etherscan.io/tx/${tx.hash}\n`
         return message
-
-    } catch (error) {
-        console.log(error);
-
-    }
-
-}
-
-export async function getPNL() {
-    try {
-        let tokenSupplied: any = await getBalanceLinkToken(configParams.LINK_ADDRESS);
-        let tokenOut: any = await getBalanceLinkToken(configParams.aLINK_ADDRESS);
-
-        const result = tokenOut - tokenSupplied;
-
-        return result;
-
     } catch (error) {
         console.log(error);
     }
 }
+
+
+
+export function getPNL(onPNLCalculated: (pnl: number) => void) {
+    // Schedule this function to run every 20 seconds
+    cron.schedule('*/20 * * * * *', async () => {
+        try {
+            let tokenSupplied: any = await getBalanceLinkToken(configParams.LINK_ADDRESS);
+            let tokenOut: any = await getBalanceLinkToken(configParams.aLINK_ADDRESS);
+            const result = tokenOut - tokenSupplied;
+            console.log('Result: ',result)
+            onPNLCalculated(result);
+        } catch (error) {
+            console.log(error);
+        }
+    });
+}
+
+
+
 
 
